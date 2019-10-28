70965365889a7b5ed51b2e5ccfd74c949f386d28
packetpimp3 <admin@packetflow.co.uk>
2019-10-28 22:41:04 +0000

    updates

--- scripts/update_netbox.py
+++ scripts/update_netbox.py
@@ -1,10 +1,11 @@
-#!./venv/bin/python
+[#!./venv/bin/python
 
 import re
 from nornir.plugins.tasks import networking
 from nornir.plugins.functions.text import print_result
 from nornir import InitNornir
 from netbox import NetBox
+import pprint
 
 nr = InitNornir(config_file="./config.yaml")
 
@@ -12,19 +13,37 @@ nb_url, nb_token, ssl_verify = nr.config.inventory.options.values()
 nb_host = re.sub("^.*//|:.*$", "", nb_url)
 
 netbox = NetBox(host=nb_host, port=32768, use_ssl=False, auth_token=nb_token)
+interfaces = netbox.dcim.get_interfaces()
+
+def is_interface_present(interfaces, device_name, interface_name):
+    print(device_name, interface_name)
+    if [
+        i
+        for i in interfaces(device=device_name)
+        if i["name"] == interface_name and i["device"]["display_name"] == device_name
+    ]:
+        print("yes")
+        return True
+    else:
+        return False
 
 
 def update_netbox_device_field(task):
-    r = task.run(task=networking.napalm_get, getters=["facts"])
-    os_version = r.result["facts"]["os_version"]
-    netbox.dcim.update_device(
-        device_name=f"{task.host}", custom_fields={"Version": os_version}
-    )
+    r = task.run(task=networking.napalm_get, getters=["interfaces"])
+    interfaces = r.result["interfaces"]
+    for interface_key in interfaces.keys():
+        mac_address = interfaces[interface_key]["mac_address"]
+        description = interfaces[interface_key]["description"]
+        if is_interface_present(f"{task.host}", interface_key):
+            netbox.dcim.update_interface(
+                device=f"{task.host}",
+                interface=interface_key,
+                description=description,
+                mac_address=mac_address,
+            )
 
 
 devices = nr.filter(role="switch")
 
-result = devices.run(
-    name="Update Netbox Device Version", task=update_netbox_device_field
-)
+result = devices.run(name="Update Netbox Interfaces", task=update_netbox_device_field)
 print_result(result, vars=["stdout"])
