6ae81ecede94dadef52edc16730bf67dfa8df221
packetpimp3 <admin@packetflow.co.uk>
2019-10-29 12:51:52 +0000

    updates

---
+++ scripts/create_netbox_interfaces.py
@@ -0,0 +1,45 @@
+#!./venv/bin/python
+
+import re
+from nornir.plugins.tasks import networking
+from nornir.plugins.functions.text import print_result
+from nornir import InitNornir
+from netbox import NetBox
+from helpers import get_device_id, is_interface_present
+import pprint
+
+nr = InitNornir(config_file="./config.yaml")
+
+nb_url, nb_token, ssl_verify = nr.config.inventory.options.values()
+nb_host = re.sub("^.*//|:.*$", "", nb_url)
+
+netbox = NetBox(host=nb_host, port=32768, use_ssl=False, auth_token=nb_token)
+nb_interfaces = netbox.dcim.get_interfaces()
+
+
+def create_netbox_interface(task, nb_interfaces, netbox):
+    r = task.run(task=networking.napalm_get, getters=["interfaces"])
+    interfaces = r.result["interfaces"]
+
+    for interface_name in interfaces.keys():
+        if not is_interface_present(nb_interfaces, f"{task.host}", interface_name):
+            print(
+                f"* Creating NB Interface : device {task.host}, interface {interface_name}"
+            )
+            device_id = get_device_id(f"{task.host}", netbox)
+            netbox.dcim.create_interface(
+                name=f"{interface_name}",
+                form_factor=1200,
+                device_id=device_id,  # default,
+            )
+
+
+devices = nr.filter(role="switch")
+
+result = devices.run(
+    name="Create Netbox Interfaces",
+    nb_interfaces=nb_interfaces,
+    netbox=netbox,
+    task=create_netbox_interface,
+)
+print_result(result, vars=["stdout"])
--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,6 +1,13 @@
-#!./venv/bin/python
+#!./venv/bin/python -i
 
 from secrets import creds
+from nornir import InitNornir
+from pprint import pprint
+from netbox import NetBox
+import argparse
+import json
+
+# Tranform Functions
 
 
 def adapt_user_password(host):
@@ -8,15 +15,30 @@ def adapt_user_password(host):
     host.password = creds[f"{host}"]["password"]
 
 
-if __name__ == "__main__":
-    from nornir import InitNornir
-    from pprint import pprint
-    import argparse
-    import json
+# Netbox
 
+
+def is_interface_present(nb_interfaces, device_name, interface_name):
+    for i in nb_interfaces:
+        if i["name"] == interface_name and i["device"]["display_name"] == device_name:
+            return True
+    return False
+
+
+def get_device_id(device_name, netbox):
+    device_id = netbox.dcim.get_devices(name=device_name)[0]["id"]
+    return device_id
+
+
+# CLI
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Nornir Helper CLI")
     parser.add_argument(
         "-i", "--inventory", action="store_true", help="show inventory", required=False
+    )
+    parser.add_argument(
+        "-n", "--netbox", action="store_true", help="netbox debug", required=False
     )
 
     args = vars(parser.parse_args())
@@ -24,3 +46,10 @@ if __name__ == "__main__":
     if args["inventory"]:
         nr = InitNornir(config_file="./config.yaml")
         pprint(nr.inventory.get_inventory_dict())
+    elif args["netbox"]:
+        netbox = NetBox(
+            host="172.29.236.139",
+            port=32768,
+            use_ssl=False,
+            auth_token="c6d10bb5a03e11120719b2a704409b4a4a7bd004",
+        )
